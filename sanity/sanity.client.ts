a4db1ef68e75e7c7fc274e9ac6d21c063e8153c8
Swappnet <advance11ua@gmail.com>
2023-11-04 09:18:18 +0200

    fixed build

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const config: ClientConfig = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "1bvfxolc",
   dataset: "production",
   apiVersion: "2023-07-16",
   useCdn: true,
