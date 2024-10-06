1db35a8128a71e79bbb771360a3261547b4d0c5d
Vladyslav Okuskov <111042761+vladokuskov@users.noreply.github.com>
2024-10-06 12:12:57 +0300

    Added dynamic current year to footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,11 +4,13 @@ import { ThemeSwitcher } from "../ui/themeSwitcher";
 
 const Footer = () => {
   const theme = cookies().get("x-theme")?.value;
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="flex flex-col items-center justify-center gap-2 mt-12 mb-4 pb-2">
       <ThemeSwitcher theme={theme === "dark" ? "dark" : "light"} />
       <p className="font-poppins text-foreground/70 text-sm font-extralight tracking-wide">
-        Vladyslav Okuskov ` 2023
+        Vladyslav Okuskov &copy; {currentYear}
       </p>
     </footer>
   );
